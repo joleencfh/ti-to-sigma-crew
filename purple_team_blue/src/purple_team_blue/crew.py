48fa957307d914f5e9c52feb43e5c2e7f19f8497
Giulia Consonni <kawngc@gmail.com>
2025-09-05 16:43:35 +0200

    removed comments

--- purple_team_blue/src/purple_team_blue/crew.py
+++ purple_team_blue/src/purple_team_blue/crew.py
@@ -29,52 +29,43 @@ class PurpleTeamBlue():
     agents: List[BaseAgent]
     tasks: List[Task]
 
-    # Learn more about YAML configuration files here:
-    # Agents: https://docs.crewai.com/concepts/agents#yaml-configuration-recommended
-    # Tasks: https://docs.crewai.com/concepts/tasks#yaml-configuration-recommended
-    
-    # If you would like to add tools to your agents, you can learn more about it here:
-    # https://docs.crewai.com/concepts/agents#agent-tools
+   
     @agent
     def intel_analyst(self) -> Agent:
         return Agent(
-            config=self.agents_config['intel_analyst'], tools=[scrape_website_tool], # type: ignore[index]
+            config=self.agents_config['intel_analyst'], tools=[scrape_website_tool], 
             verbose=True
         )
 
     @agent
     def detection_engineer(self) -> Agent:
         return Agent(
-            config=self.agents_config['detection_engineer'], tools=[github_search_tool], # type: ignore[index]
+            config=self.agents_config['detection_engineer'], tools=[github_search_tool], 
             verbose=True
         )
 
-    # To learn more about structured task outputs,
-    # task dependencies, and task callbacks, check out the documentation:
-    # https://docs.crewai.com/concepts/tasks#overview-of-a-task
+    
     @task
     def cti_analysis_task(self) -> Task:
         return Task(
-            config=self.tasks_config['cti_analysis_task'], # type: ignore[index]
+            config=self.tasks_config['cti_analysis_task'], 
         )
 
     @task
     def initial_rule_creation_task(self) -> Task:
         return Task(
-            config=self.tasks_config['initial_rule_creation_task'], # type: ignore[index]
+            config=self.tasks_config['initial_rule_creation_task'], 
             output_file='sigma.yml'
         )
 
     @crew
     def crew(self) -> Crew:
         """Creates the TestCrew crew"""
-        # To learn how to add knowledge sources to your crew, check out the documentation:
-        # https://docs.crewai.com/concepts/knowledge#what-is-knowledge
+        
 
         return Crew(
-            agents=self.agents, # Automatically created by the @agent decorator
-            tasks=self.tasks, # Automatically created by the @task decorator
+            agents=self.agents, 
+            tasks=self.tasks, 
             process=Process.sequential,
             verbose=True,
-            # process=Process.hierarchical, # In case you wanna use that instead https://docs.crewai.com/how-to/Hierarchical/
         )
